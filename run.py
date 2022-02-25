7070569b2b2ba2fae717615c7fe88b11e60df3c7
Tmpts21 <randel@metalmanagementsolutions.com>
2022-02-25 15:10:32 +0800

    first run

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
-import turtle 
+import turtle     
+from sympy.ntheory.primetest import isprime 
+
+
+t = turtle.Turtle()      
+side = 5 
+step = 1  
+nums = 1   
+t.hideturtle()
+for i in range (200) :        
+    for j in range (step) :     
+        if isprime(nums) :   
+            t.dot(4)  
+        t.forward(4)     
+        nums += 1 
+    t.left(90)
+    step += 1   
+    nums += 1  
+
+t.clear() 
+
+   
+
+input() 
+
+
 
